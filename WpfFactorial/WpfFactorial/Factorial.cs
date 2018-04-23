36ca7ff2922fa7b004a3fd98f1970c5c9cfc6fe8
Milan <milgurung@gmail.com>
2018-04-23 19:17:30 +0100

    Added Factorial.cs

---
+++ WpfFactorial/WpfFactorial/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfFactorial
+{
+    public class Factorial
+    {
+        public int DoCalc(int number)
+        {
+            var fact = 1;
+            if (number == 0)
+                return fact;
+            for(var i = number; i>0;i--)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}
